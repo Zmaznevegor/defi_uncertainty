5b1af08052e5016699f54ddcac5f4879b6b502e3
Zmaznevegor <zmaznevegor@gmail.com>
2020-10-30 00:53:03 +0100

    Fixed loop for dcrpt scrapper + results as dataframe

--- news_data.py
+++ news_data.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import time
+from random import randrange
 
 # Data wrangling
 import pandas as pd
@@ -20,7 +21,7 @@ def collect_data(wp):
     category = []
 
     # same range as posts per page
-    for j in range(0, 250):
+    for j in range(0, len(wp)):
         date.append(wp[j]['date'])
         content_text.append(wp[j]['custom_fields']['content_text'])
         # category.append(webpage[j]['custom_fields']['primary_category']['slug'])
@@ -35,15 +36,15 @@ def collect_data(wp):
 
 
 # step equals to the results per page
-offsets = np.arange(0, 6250, 250).tolist()
+offsets = np.arange(0, 6250, 25).tolist()
 frames = []
 
 # Loop that goes through all the available news by shifting the offset
 for i in offsets:
-    time.sleep(10)
+    time.sleep(randrange(5))
     print(i)
-    cmc = requests.get(f'https://api.decrypt.co/content/posts?_minimal=true&category=news&lang=en-US&offset={i}&order=desc&orderby=date&per_page=250')
-    time.sleep(60)
+    cmc = requests.get(f'https://api.decrypt.co/content/posts?_minimal=true&category=news&lang=en-US&offset={i}&order=desc&orderby=date&per_page=25')
+    time.sleep(5)
     webpage = cmc.json()
     df = collect_data(webpage)
     frames.append(df)
