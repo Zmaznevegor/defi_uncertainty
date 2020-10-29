2a27ec32f3d933a90efb6f70541b787b39932465
Zmaznevegor <zmaznevegor@gmail.com>
2020-10-29 22:04:56 +0100

    Function for the data filtering and for loop for the data extraction of 6k news.

--- news_data.py
+++ news_data.py
@@ -12,110 +12,60 @@ import dill
 # Define folder
 data_folder = r'/home/zmaznevegor/PycharmProjects/defi_uncertainty/data'
 
+
 # Data collection
-# TODO: Decrypt website
-
-
-
-# TODO: define function that saves the descriptions and timestamps
-# TODO: define time breaks
-# TODO: construct a dataframe
-
-cmc = requests.get(
-    'https://api.decrypt.co/content/posts?_minimal=true&category=news&lang=en-US&offset=0&order=desc&orderby=date&per_page=250')
-webpage = cmc.json()
-
-offsets = np.arange(0, 6250, 250).tolist()
-
-
-def collect_data(cmc):
-
-    # Collect historical data
+def collect_data(wp):
     date = []
     content_text = []
-    category_slug = []
+    category = []
 
-    for i in coins:
-        time.sleep(3)
-        cmc = requests.get(f'https://api.decrypt.co/content/posts?_minimal=true&category=news&lang=en-US&offset={offsets[i]}&order=desc&orderby=date&per_page=250')
-        time.sleep(3)
-        webpage = cmc.json()
+    # same range as posts per page
+    for j in range(0, 250):
+        date.append(wp[j]['date'])
+        content_text.append(wp[j]['custom_fields']['content_text'])
+        # category.append(webpage[j]['custom_fields']['primary_category']['slug'])
 
-        historical_data = json.loads(data.contents[0])
-        quotes = historical_data['props']['initialState']['cryptocurrency']['ohlcvHistorical'][i]['quotes']
-        info = historical_data['props']['initialState']['cryptocurrency']['info']['data'][i]
+    df = pd.DataFrame(columns=['date', 'text'])
 
-        name.append(info['name'])
-        symbol.append(info['symbol'])
-        slug.append(info['slug'])
-        f_desc.append(info['description'])
-        subreddit.append(info['urls']['reddit'])
-        code.append(info['urls']['source_code'])
-        category.append(info['category'])
-        status.append(info['status'])
+    df['date'] = date
+    df['text'] = content_text
+    # df['category'] = category
 
-        for j in quotes:
-            market_cap.append(j['quote']['USD']['market_cap'])
-            volume.append(j['quote']['USD']['volume'])
-            timestamp.append(j['quote']['USD']['timestamp'])
-
-        df_hist = pd.DataFrame(columns=['marketcap',
-                                        'volume',
-                                        'timestamp'])
-
-        df_hist['marketcap'] = market_cap
-        df_hist['volume'] = volume
-        df_hist['timestamp'] = timestamp
-
-        hist[coins[i]] = df_hist
-
-    df = pd.DataFrame(columns=['name',
-                               'symbol',
-                               'slug',
-                               'description',
-                               'subreddit',
-                               'code',
-                               'category',
-                               'status'])
-
-    df['name'] = name
-    df['symbol'] = symbol
-    df['slug'] = slug
-    df['description'] = f_desc
-    df['subreddit'] = subreddit
-    df['code'] = code
-    df['category'] = category
-    df['status'] = status
-
-    return df, hist
+    return df
 
 
-# For the first page
-data = list(collect_data(cmc))
-
-# Scrapping data for the rest of the pages
-for i in range(2, 23):
-    url = f'https://coinmarketcap.com/{i}/'
-    cmc = requests.get(url)
-    time.sleep(3)
-    page_content = list(collect_data(cmc))
-    time.sleep(3)
-    data.append(page_content[0])
-    data.append(page_content[1])
-
-# Save textual descriptions
+# step equals to the results per page
+offsets = np.arange(0, 6250, 250).tolist()
 frames = []
-for i in range(0, 44):
-    if type(data[i]) == pd.DataFrame:
-        frames.append(data[i])
 
+# Loop that goes through all the available news by shifting the offset
+for i in offsets:
+    time.sleep(10)
+    print(i)
+    cmc = requests.get(f'https://api.decrypt.co/content/posts?_minimal=true&category=news&lang=en-US&offset={i}&order=desc&orderby=date&per_page=250')
+    time.sleep(60)
+    webpage = cmc.json()
+    df = collect_data(webpage)
+    frames.append(df)
+
+# Combining and exporting all the results
 result = pd.concat(frames, ignore_index=True)
-result.to_csv(data_folder+'/crypto.csv', index= False)
-
+result.to_csv(data_folder + '/decrypt.csv', index=False)
 
 # TODO: Coindesk website data
-cmc = requests.get('https://headlines-api.coinmarketcap.com/articles?page=0&size=20&type=news')
+cmc = requests.get(f'https://www.coindesk.com/wp-json/v1/articles/format/news/{1100}?mode=list', stream=True)
 webpage = cmc.json()
 
+# TODO: CCN scrapper
+cmc = requests.get(f'https://www.coindesk.com/wp-json/v1/articles/format/news/{1100}?mode=list', stream=True)
+webpage = cmc.json()
+
+# TODO: The Block scrapper
+cmc = requests.get(f'https://www.coindesk.com/wp-json/v1/articles/format/news/{1100}?mode=list', stream=True)
+webpage = cmc.json()
+
+# TODO: Modern Consensus scrapper
+cmc = requests.get(f'https://www.coindesk.com/wp-json/v1/articles/format/news/{1100}?mode=list', stream=True)
+webpage = cmc.json()
 
 # TODO: pkl data
