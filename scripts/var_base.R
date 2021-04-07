23bac4bf6742d03e81cec7d71727daae26830544
Zmaznevegor <zmaznevegor@gmail.com>
2021-04-07 03:16:01 +0200

    Plotted VAR and preliminary found couple of the suitable models

--- scripts/var_base.R
+++ scripts/var_base.R
@@ -9,32 +9,21 @@ library(vars)
 
 theme_set(theme_minimal())
 
-z <- readr::read_csv("data/weekly_epu_base.csv")
-
-z <- z %>% 
-  mutate(week = yearweek(date)) %>% 
-  select(-date) %>% 
-  as_tsibble(key = epu, index = week)
-
-?yearweek
-# Combine data sets
-z <- right_join(z, maker, by = "date")
-
-y <- ts(z[,3:5], frequency=53,
+a <- ts(inp[,2:7], frequency=53,
         start= c(2017,52),
-        end = c(2020,44))
+        end = c(2021,11))
 
-autoplot(y)
+autoplot(a)
 
 # Plot EPU Index
-autoplot(y[,1])+
+autoplot(a[,1])+
   ggtitle("Weekly EPU for DeFi services") +
   ylab("Index") + xlab("Week")
 
 # Check differencing and log
-diff(y[,3]) %>% checkresiduals()
+diff(a[,3]) %>% checkresiduals()
 
-diffed <- diff(y)
+diffed <- diff(a)
 
 autoplot(diffed[,1])+
   ggtitle("Differenced weekly EPU") +
@@ -44,21 +33,66 @@ autoplot(diff(log(y[,3])))+
   ggtitle("Differenced weekly Maker price") +
   ylab("Price (ETH)") + xlab("Week")
 
-diff(log(y[,2])) %>% checkresiduals()
+diff(log(a[,2])) %>% checkresiduals()
 
 # Select best VAR ----
 # For Maker
-VARselect(cbind(epu = y[,1][2:length(y[,2])], 
-                diff_tvl = diff(log(y[,2]))),
+VARselect(cbind(epu = a[,1], 
+                tvl = log(a[,5])),
           type="both")[["selection"]]
 
-var <- VAR(cbind(epu = y[,1][2:length(y[,2])], 
-                 diff_tvl = diff(log(y[,2]))),
-           p=1, type="const")
+var <- VAR(cbind(epu =a[,1], 
+                 tvl = log(a[,5])), 
+           p=3, 
+           type="both")
 
 # Impulse response functions
-irf <- irf(var, impulse = "epu", response = "diff_tvl", 
-               n.ahead = 10, boot = TRUE)
+irf <- irf(var, impulse = "epu", response = "tvl", 
+               n.ahead = 12, boot = TRUE, runs = 100)
 
 plot(irf, ylab = "output", main = "Shock from uncertainty")
 
+irf[["irf"]][["epu"]] %>% as.data.frame() %>% 
+  ggplot(aes(y = tvl, x = c(1:13)))+
+  geom_line(linetype = 2, colour = "black", alpha = 1, size = 0.75)+
+  geom_line(data = irf[["Lower"]][["epu"]] %>% as.data.frame(), aes(y = tvl, x =c(1:13)), linetype = 2, colour = "red", alpha = 0.8, size = 0.4)+
+  geom_line(data = irf[["Upper"]][["epu"]] %>% as.data.frame(), aes(y = tvl, x =c(1:13)), linetype = 2, colour = "red", alpha = 0.8, size = 0.4)+
+  geom_hline(yintercept = 0, linetype = "dotted", colour = "black", alpha = 0.5)+
+  coord_cartesian(ylim = c(-5, 6))+
+  scale_y_continuous(breaks = seq(-6, 6, by = 1))+
+  scale_x_continuous(breaks = seq(0, 12, by = 2))+
+  labs(subtitle = "Modified DeFi EPU index", title = "Effect of uncertainty impulse on verified contracts",
+       x = "Lag", y = "") +
+  theme(plot.title = element_text(hjust = 0.5, vjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5, vjust = 0.5))
+
+# TODO: try with summed verified transactions
+
+VARselect(cbind(epu = a[,1], 
+                tvl = a[,2]),
+          type="both")[["selection"]]
+
+var <- VAR(cbind(epu = a[,1], 
+                 tvl = a[,2]), 
+           p=4, 
+           type="both")
+
+# Impulse response functions
+irf <- irf(var, impulse = "epu", response = "tvl", 
+           n.ahead = 12, boot = TRUE, runs = 1500)
+
+plot(irf, ylab = "output", main = "Shock from uncertainty")
+
+irf[["irf"]][["epu"]] %>% as.data.frame() %>% 
+  ggplot(aes(y = tvl, x = c(1:13)))+
+  geom_line(linetype = 2, colour = "black", alpha = 1, size = 0.75)+
+  geom_line(data = irf[["Lower"]][["epu"]] %>% as.data.frame(), aes(y = tvl, x =c(1:13)), linetype = 2, colour = "red", alpha = 0.8, size = 0.4)+
+  geom_line(data = irf[["Upper"]][["epu"]] %>% as.data.frame(), aes(y = tvl, x =c(1:13)), linetype = 2, colour = "red", alpha = 0.8, size = 0.4)+
+  geom_hline(yintercept = 0, linetype = "dotted", colour = "black", alpha = 0.5)+
+  coord_cartesian(ylim = c(-5, 6))+
+  scale_y_continuous(breaks = seq(-6, 6, by = 1))+
+  scale_x_continuous(breaks = seq(0, 12, by = 2))+
+  labs(subtitle = "Modified DeFi EPU index", title = "Effect of uncertainty impulse on verified contracts",
+       x = "Lag", y = "") +
+  theme(plot.title = element_text(hjust = 0.5, vjust = 0.5),
+                                  plot.subtitle = element_text(hjust = 0.5, vjust = 0.5))
